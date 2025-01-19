91890bf748f19b9f16b0f477417debc8189a30c2
ketrololz <bqnexxx@gmail.com>
2025-01-19 20:21:38 +0300

    refactor: 04-copy-directory add function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,36 +4,44 @@ const path = require('path');
 const cloneFolderPath = path.join(__dirname, 'files-copy');
 const authFolderPath = path.join(__dirname, 'files');
 
-fs.mkdir(path.join(cloneFolderPath), { recursive: true }, (err) => {
-  if (err) throw err;
-});
-
-fs.readdir(path.join(authFolderPath), (err, files) => {
-  if (err) throw err;
-
-  files.forEach((file) => {
-    fs.copyFile(
-      path.join(authFolderPath, file),
-      path.join(cloneFolderPath, file),
-      (err) => {
-        if (err) return err;
-      },
-    );
-  });
-});
-
-fs.readdir(path.join(cloneFolderPath), (err, files) => {
-  if (err) throw err;
-
-  files.forEach((file) => {
-    fs.open(path.join(authFolderPath, file), 'r', (err) => {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          fs.unlink(path.join(cloneFolderPath, file), (err) => {
-            if (err) throw err;
-          });
-        }
-      }
+function copyDir() {
+  try {
+    fs.mkdir(path.join(cloneFolderPath), { recursive: true }, (err) => {
+      if (err) throw err;
     });
-  });
-});
+
+    fs.readdir(path.join(authFolderPath), (err, files) => {
+      if (err) throw err;
+
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(authFolderPath, file),
+          path.join(cloneFolderPath, file),
+          (err) => {
+            if (err) return err;
+          },
+        );
+      });
+    });
+
+    fs.readdir(path.join(cloneFolderPath), (err, files) => {
+      if (err) throw err;
+
+      files.forEach((file) => {
+        fs.open(path.join(authFolderPath, file), 'r', (err) => {
+          if (err) {
+            if (err.code === 'ENOENT') {
+              fs.unlink(path.join(cloneFolderPath, file), (err) => {
+                if (err) throw err;
+              });
+            }
+          }
+        });
+      });
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyDir();
